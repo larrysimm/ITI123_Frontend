c756912b0a8da3550cdb96201070eee16f09b48e
Larry <sim.larry88@gmail.com>
2026-01-04 16:11:55 +0800

    fix typo

--- src/hooks/useInterviewSession.js
+++ src/hooks/useInterviewSession.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { a } from "framer-motion/client";
 
 export function useInterviewSession(apiUrl, serverStatus, targetRole) {
   // --- Upload State ---
