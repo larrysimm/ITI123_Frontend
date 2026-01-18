d458ed289f362bb719ff1f68abf141fc1a9e9c2f
Larry <sim.larry88@gmail.com>
2026-01-18 12:39:25 +0800

    move the logo up

--- src/components/LandingScreen.js
+++ src/components/LandingScreen.js
@@ -39,7 +39,7 @@ export default function LandingScreen({
   };
 
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center min-vh-100 py-5 text-center">
+    <div className="d-flex flex-column align-items-center justify-content-center-start min-vh-100 py-5 text-center">
       <div className="mb-4">
         <img
           src={logo}
